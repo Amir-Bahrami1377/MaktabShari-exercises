8c4aab49b1e7b9141b81544ed864496d7ce16db3
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2022-02-05 00:30:26 +0330

    charfield max length added

--- salon/services/models.py
+++ salon/services/models.py
@@ -3,13 +3,13 @@ from core.models import BaseModel
 
 
 class Weekend(models.Model):
-    off_days = models.CharField()
+    off_days = models.CharField(max_length=50)
     open_at = models.TimeField()
     close_at = models.TimeField()
 
 
 class Service(BaseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     cost = models.IntegerField()
     duration = models.TimeField()
 
--- salon/users/models.py
+++ salon/users/models.py
@@ -4,12 +4,12 @@ from services.models import Service
 
 
 class Customer(BaseModel):
-    username = models.CharField()
+    username = models.CharField(max_length=30)
     email = models.EmailField()
-    password = models.CharField()
+    password = models.CharField(max_length=30)
 
 
 class Agent(BaseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     is_available = models.BooleanField(default=True)
     expertise = models.ForeignKey(Service, on_delete=models.CASCADE)
